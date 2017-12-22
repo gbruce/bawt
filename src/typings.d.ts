83e4f91faba17b35ae0fb216c7ac6781106e22b9
Guillermo Bruce <gbruce@gmail.com>
2017-12-22 01:44:58 -0800

    fix crypto/bignum add type declarations

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -7,3 +7,18 @@ declare module "*.json" {
   const value: any;
   export default value;
 }
+
+declare class BigInteger {
+  constructor(a: any, b: any, c?: any);
+}
+
+declare module BigInteger {
+  export let ZERO: any;
+  export function fromInt(num: number): void;
+}
+
+declare module 'jsbn/lib/big-integer' {
+  const value: BigInteger;
+  export = BigInteger;
+}
+
