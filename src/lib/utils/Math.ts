a02687789dcb97398337d41e332d6012003f43ae
Guillermo Bruce <gbruce@gmail.com>
2018-12-31 02:05:28 -0800

    liiint

--- src/lib/utils/Math.ts
+++ src/lib/utils/Math.ts
@@ -3,10 +3,10 @@ import { Vector3 } from 'three';
 
 export const MakeVector3 = (x: number, y: number, z: number): IVector3 => {
   return { x, y, z };
-}
+};
 
 export const CopyToVector3 = (fromVector: IVector3, toVector: Vector3) => {
   toVector.x = fromVector.x;
   toVector.y = fromVector.y;
   toVector.z = fromVector.z;
-}
+};
